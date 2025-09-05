615162627b94fe9b6ceebdc6d03a1ea3a229d904
Vlad <vladguzun20022@gmail.com>
2025-09-05 13:54:19 +0000

    jjj

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,3 +5,10 @@ def test_health():
         r = c.get("/health")
         assert r.status_code == 200
         assert r.get_json()["ok"] is True
+
+def test_orice():
+    with app.test_client() as c:
+        r = c.get("/")
+        assert r.status_code == 200
+        assert r.get_json()["name"] == "Vlad"
+
